46a6b797cda73284fe831b47ec3fa9aa390588ca
Vetsen86 <erik.beckstrand@gmail.com>
2020-05-16 14:19:47 -0600

    routing and pages added; navbar prelim styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 import './App.css';
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <h1>Hello World</h1>
+      <Router>
+        <Navbar></Navbar>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,21 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-sm navbar-light bg-light">
-                <a class="navbar-brand" href="#">Erik Beckstrand</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-sm">
+                <a className="navbar-brand" target="_blank" href="https://www.linkedin.com/in/erik-beckstrand-1b1a8818a/">Erik Beckstrand</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/">
+                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                            </Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li className="nav-item">
+                            <Link to="/portfolio">
+                                <a className="nav-link" href="/portfolio">Portfolio</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact">
+                                <a className="nav-link" href="/contact">Contact</a>
+                            </Link>
                         </li>
                     </ul>
 
