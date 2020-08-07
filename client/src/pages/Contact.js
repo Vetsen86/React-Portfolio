e49dd5381826419371f7a1f522b543355d0f113a
Vetsen86 <erik.beckstrand@gmail.com>
2020-08-07 13:49:31 -0600

    contact page mostly finished, needs some styyyyyle

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <h1>Contact Page</h1>
+        <div className="container contact">
+            <h1>Contact Info</h1>
+            <p><strong>Email Address:</strong></p>
+            <p>erik.beckstrand@gmail.com</p>
+            <p><strong>Phone Number:</strong></p>
+            <p>385-227-0865</p>
+            <a className="btn btn-primary contact-link" href="https://github.com/Vetsen86" target="_blank">Github</a>
+            <a className="btn btn-primary contact-link" href="https://www.linkedin.com/in/erik-beckstrand-1b1a8818a/" target="_blank">LinkedIn</a>
+        </div>
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                     <img id="me" src="./me.jpg" />
                 </div>
                 <div className="col-8">
-                    <p>Hi, my name is Jon Erik Beckstrand, though I go by my middle name, Erik. I'm 31 years old, although I know that I look about 20, at the most. I was born in Salt Lake City, and have been here my whole life. My dream, however, is to move somewhere else in the country to start, and then possibly move out of the country. At the very least I'd like to travel to as many countries as I possibly can.</p>
+                    <p>Hi, my name is Jon Erik Beckstrand, though I go by my middle name, Erik. I'm 32 years old, although I know that I look about 20, at the most. I was born in Salt Lake City, and have been here my whole life. My dream, however, is to move somewhere else in the country to start, and then possibly move out of the country. At the very least I'd like to travel to as many countries as I possibly can.</p>
 
 
                     <p>My interests and hobbies are all pretty nerdy. I guess you could say I'm a stereotypical nerd. Video games are by far my favorite, and I spend most of my free time and extra cash on them. I also love movies, especially Quentin Tarantino. I'm a big animal lover and I really wish I had the extra cash for a pet, but they're so darn expensive! I looooooooove food, especially anything Asian.</p>
@@ -17,7 +17,7 @@ const Home = () => {
 
                     <p>As far as coding goes, I'm not afraid to admit that I'm a beginner. I had some self-taught experience prior to entering the bootcamp, but nothing to write home about. I am finding that little bit of knowledge to be invaluable, however. It would certainly be much harder to keep up without it. I'm hoping to secure a job as a web developer once I graduate, mostly because I enjoy coding but also because it pays quite well!</p><br></br>
 
-                    <a className="btn btn-primary" href="#" target="_blank">My Resume</a>
+                    <a className="btn btn-primary" href="https://docs.google.com/document/d/10X0Sa-ivegEu0MiVMPskheyuaER5BqlA6aTHPkvfCLo/edit?usp=sharing" target="_blank">My Resume</a>
 
                 </div>
             </div>
