ae4c51794cfa1f64913d16fc24933fef50b00c74
Vetsen86 <erik.beckstrand@gmail.com>
2020-09-19 16:38:58 -0600

    Portfolio Finished!

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -22,7 +22,7 @@ const Portfolio = () => {
                     </div>
                 </div>
                 <div className="col-4">
-                    <img className="portfolio-image" src="./news-scraper.png" />
+                    <img className="portfolio-image" src="./nodejs.png" />
                     <h3 className="text-center">Bamazon</h3>
                     <div className="button-container">
                         <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/bamazon">Github Repository</a>
@@ -32,30 +32,111 @@ const Portfolio = () => {
 
             <div className="row">
                 <div className="col-4">
-                    <img className="portfolio-image" src="./news-scraper.png" />
-                    <h3 className="text-center">News Scraper</h3>
+                    <img className="portfolio-image" src="./whatsinmypantry.png" />
+                    <h3 className="text-center">Final Project: What's in my Pantry (React)</h3>
                     <div className="button-container">
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://news-scraper-erikb.herokuapp.com/">Deployed Version</a>
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/News-Scraper">Github Repository</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://whats-in-my-pantry.herokuapp.com/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/PrestonWeber/WhatsInMyPantryReact">Github Repository</a>
                     </div>
                 </div>
                 <div className="col-4">
-                    <img className="portfolio-image" src="./news-scraper.png" />
-                    <h3 className="text-center">News Scraper</h3>
+                    <img className="portfolio-image" src="./nodejs.png" />
+                    <h3 className="text-center">LIRI node bot</h3>
                     <div className="button-container">
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://news-scraper-erikb.herokuapp.com/">Deployed Version</a>
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/News-Scraper">Github Repository</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/liri-node-app">Github Repository</a>
                     </div>
                 </div>
                 <div className="col-4">
-                    <img className="portfolio-image" src="./news-scraper.png" />
-                    <h3 className="text-center">News Scraper</h3>
+                    <img className="portfolio-image" src="./burgers.png" />
+                    <h3 className="text-center">Eat the burger</h3>
                     <div className="button-container">
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://news-scraper-erikb.herokuapp.com/">Deployed Version</a>
-                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/News-Scraper">Github Repository</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://eat-the-burger-erikb.herokuapp.com/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/burger">Github Repository</a>
                     </div>
                 </div>
             </div>
+
+            <div className="row">
+                <div className="col-4">
+                    <img className="portfolio-image" src="./friend.png" />
+                    <h3 className="text-center">Friend Finder</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://friend-finder-erikb.herokuapp.com/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/FriendFinder">Github Repository</a>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <img className="portfolio-image" src="./pantry.png" />
+                    <h3 className="text-center">Project 1: What's in my Pantry (Non-React)</h3>
+                    <div className="button-container">
+                        
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://collinchiz.github.io/Project-1/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/CollinChiz/Project-1">Github Repository</a>
+                        
+                    </div>
+                </div>
+                <div className="col-4">
+                    <img className="portfolio-image" src="./train.png" />
+                    <h3 className="text-center">Train Scheduler</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/Train-Scheduler/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/Train-Scheduler">Github Repository</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4">
+                    <img className="portfolio-image" src="./giftastic.png" />
+                    <h3 className="text-center">Giftastic</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/Giftastic/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/Giftastic">Github Repository</a>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <img className="portfolio-image" src="./nytarticles.png" />
+                    <h3 className="text-center">New York Times Article Search</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/NewYorkTimesArticles/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/NewYorkTimesArticles">Github Repository</a>
+                        
+                    </div>
+                </div>
+                <div className="col-4">
+                    <img className="portfolio-image" src="./trivia.png" />
+                    <h3 className="text-center">Trivia Game</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/TriviaGame/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/TriviaGame">Github Repository</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-4">
+                    <img className="portfolio-image" src="./crystals.png" />
+                    <h3 className="text-center">Crystal Collector</h3>
+                    <div className="button-container">
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/unit-4-game/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/unit-4-game">Github Repository</a>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <img className="portfolio-image" src="./word.png" />
+                    <h3 className="text-center">Word Guess Game</h3>
+                    <div className="button-container">
+                    <a className="btn btn-primary portfolio-button" target="_blank" href="https://vetsen86.github.io/Word-Guess-Game/">Deployed Version</a>
+                        <a className="btn btn-primary portfolio-button" target="_blank" href="https://github.com/Vetsen86/Word-Guess-Game">Github Repository</a>
+                        
+                    </div>
+                </div>
+                <div className="col-4">
+                    
+                </div>
+            </div>
+
+
         </div>
     );
 }
